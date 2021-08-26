7f7e32226d04ea78add9a27dd62066c90b8ce741
Kyle Shu <shujunyi@gmail.com>
2021-08-26 13:18:55 +0800

    flush

--- ucx.c
+++ ucx.c
@@ -260,15 +260,6 @@ void bench(char * sdata, char * mybuff, int iter, int warmup, size_t data_size)
     memset(one_mem, 1, data_size);
     memset(zero_mem, 0, data_size);
 
-    if (memcmp(mybuff, zero_mem, data_size) == 0) {
-        if (my_pe == 0) {
-            puts("client memory OK1");
-        }
-        else {
-            puts("server memory OK1");
-        }
-    }
-
     /* provide a warmup between endpoints */
     for (int i = 0; i < warmup; i++) {
         if (my_pe == 0) {
@@ -291,16 +282,6 @@ void bench(char * sdata, char * mybuff, int iter, int warmup, size_t data_size)
 
     barrier();
     /* TODO: change this code to perform ping-pong latency */
-    //memset(mybuff, 0, HUGEPAGE);
-
-    if (memcmp(mybuff, zero_mem, data_size) == 0) {
-        if (my_pe == 0) {
-            puts("client memory OK2");
-        }
-        else {
-            puts("server memory OK2");
-        }
-    }
 
     if (my_pe == 0) {
         start = MPI_Wtime();
